c89a080425a5f0620943d87bcd7aa4285cca9320
Darian T. Yang <dty7@pitt.edu>
2022-03-03 14:28:41 -0500

    directory was missing on data file in python call

--- gamd_reweight.sh
+++ gamd_reweight.sh
@@ -49,7 +49,7 @@ elif [ $1 = "rw" ] ; then
     # perform reweighting of GaMD data
     # args : Emax (kcal/mol), cutoff (kcal/mol), binx, biny, data, T, TODO:xdim, ydim
     #bash py_reweighting/reweight-2d.sh 100 100 6 6 c2_xrms.tsv $T
-    python3 py_reweighting/PyReweighting-2D.py -input c2_xrms.tsv -T 298 -Emax 100 -cutoff 100 -discX 6 -Xdim 0 100 -discY 6 -Ydim 0 15 -job amdweight_CE -weight data/weights.dat
+    python3 py_reweighting/PyReweighting-2D.py -input data/c2_xrms.tsv -T 298 -Emax 100 -cutoff 100 -discX 6 -Xdim 0 100 -discY 6 -Ydim 0 15 -job amdweight_CE -weight data/weights.dat
 
 else
     echo "ARG 1 MUST BE 'calc', 'prep', or 'rw'"
