5495942d884f154ca3c6a8846c65abe625e0550a
Darian T. Yang <dty7@pitt.edu>
2022-03-09 17:27:38 -0500

    updated plot_pdist script

--- plot_pdist.py
+++ plot_pdist.py
@@ -15,11 +15,22 @@ def plot_pre_rw_pdist(file):
     """
 
     data = np.loadtxt(file)
-    Z, xedges, yedges = np.histogram2d(data[:,0], data[:,1], bins=100)
+    print(np.shape(data[::10,0]))
+    Z, xedges, yedges = np.histogram2d(data[::10,0], data[::10,1], bins=100)
     # normalize the pdist (-ln(P/P(max)))
     Z = -np.log(np.divide(Z, np.max(Z)))
+    plt.figure(figsize=(5,4))
     plt.pcolormesh(xedges, yedges, Z.T)
-    plt.colorbar()
+    cbar = plt.colorbar()
+    plt.clim(vmin=0, vmax=4)
+    cbar.set_label("-ln P(x)")
+
+    plt.title("2KOD GaMD - 200ns")
+    plt.xlim(20,90)
+    plt.xlabel("Helical Angle (°)")
+    plt.ylim(3,8)
+    plt.ylabel("RMSD to Alt PDB ($\AA$)")
+
     plt.show()
 
 
