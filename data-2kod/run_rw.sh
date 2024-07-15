552674bf0eeafd155ff3788a9c99dc2032cda82d
Darian T. Yang <dty7@pitt.edu>
2024-07-15 14:55:57 -0400

    ran MC and CE reweighting

--- data-2kod/run_rw.sh
+++ data-2kod/run_rw.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# perform Maclaurin Series reweighting of GaMD data (eqivalent to 1st order CE)
+# MC: perform Maclaurin Series reweighting of GaMD data (equivalent to 1st order CE)
+# CE: 2nd order cumulant expansion reweighting
 # args : Emax (kcal/mol), cutoff (min n configs in a hist bin), binx, biny, data, T
 # disc is the bin discretization interval
-python ../py_reweighting/PyReweighting-2D.py -input agg_oas.dat -T 298 -Emax 30 -cutoff 10 -discX 0.75 -Xdim 0 75 -discY 0.75 -Ydim 0 75 -job amdweight_MC -weight agg_weights.dat
+python ../py_reweighting/PyReweighting-2D.py -input agg_oas.dat -T 298 -Emax 15 -cutoff 10 -discX 0.75 -Xdim 0 75 -discY 0.75 -Ydim 0 75 -job amdweight_CE -weight agg_2kod_weights.dat
